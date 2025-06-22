1ce5e4267b18faad794e13bf8dadeda3114dea69
SilviaBaerchen <90452439+SilviaBaerchen@users.noreply.github.com>
2025-06-22 12:34:21 +0200

    fix: add workaround redirect issue

--- InteractiveSegmentation/urls.py
+++ InteractiveSegmentation/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+#from django.views.generic import RedirectView  # workaround for the redirect issue
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("home/", include("home.urls", namespace="home")),
     path("experiment/", include("experiment.urls", namespace="experiment")),
-    path("instructions/", include("instructions.urls", namespace="instructions"))
+    path("instructions/", include("instructions.urls", namespace="instructions")),
+    #path('', RedirectView.as_view(url='/home/', permanent=False))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
